b54201c856b3762db93a7151c091310a2e0629a3
Martin Rykfors <martinrykfors@gmail.com>
2015-03-27 19:44:16 +0000

    Move sequencer to the pattern namespace

--- src/wigh_figh/experiments/generic-sequencer.clj
+++ src/wigh_figh/experiments/generic-sequencer.clj
@@ -5,50 +5,42 @@
 
 (defsynth hit [note 40]
   (let [base-freq (midicps note)]
-    (->> (saw:ar (* 3 base-freq))
-         (* 3)
-         (* (env-gen (env-perc 0 0.5)))
+    (->> (sin-osc (* 14 base-freq))
+         (* 4)
+         (* (env-gen (env-perc 0 0.1)))
+         (sin-osc (* 3 base-freq))
+         (* 2)
+         (* (env-gen (env-perc 0 1.3)))
          (sin-osc base-freq)
-         (* (env-gen (env-perc 0 0.3) :action FREE))
-         (#(lpf % 3000))
+         (* (env-gen (env-perc 0 1.9) :action FREE))
+         (#(lpf % 2000))
          (out [0 1]))))
 
 (defsynth tick []
   (->> (white-noise)
-       (#(lpf % 10000))
-       (#(hpf % 3000))
+       (#(lpf % 19000))
+       (#(hpf % 8000))
        (* (env-gen (env-perc 0 0.1) :action FREE))
        (out [0 1])))
 
 (defsynth kick []
-  (->> (sin-osc (+ 30 (* 300 (env-gen (env-perc 0 0.5)))))
-       (* (env-gen (env-perc 0 0.3) :action FREE))
+  (->> (sin-osc (+ 30 (* 600 (env-gen (env-perc 0 0.1)))))
+       (* (env-gen (env-perc 0 1.3) :action FREE))
        (out [0 1])))
 
 (kick)
 (hit)
 (tick)
 
+(defn random-unit []
+  (vec (take 8 (repeatedly #(rand-int 2)))))
 (def generators (atom []))
 (reset! generators [
-                     [(pattern
-                       (rot-rec [[1 0 1 0] 2 0 [1 0 0 1]] 0))
-                      (fn []   (doseq [note (chord :c3 :minor) t [0 ]] (hit (+ t note)))) ]
-                     [(pattern [1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 ]) kick ]
-                     [(pattern [[1 0 2 1] [0 0 0 2] 3 1]) tick]
+                     [(pattern [[1 0 0 0] [0 1 0 0] [0 0 1 0] 0 2 1 0 1 ])
+                      (fn []  (doseq [note (chord :c3 :minor)] (hit note))) ]
+                     [(pattern [4]) kick ]
+                     [(pattern [[0 1 1 0] [0 1 1 0] [0 1 1 0] 4]) tick ]
                      ])
-;todo: use references to generator list
-;add mechanism for not generating infinite seqs
 
-(defn b-seq [time num-beats measure-length gen]
-  (let [next-time (+ measure-length time)]
-    (doseq [[seq-gen trigger-f] @gen]
-     (try 
-       (let [trigger-times (take-while #(< % num-beats) (seq-gen num-beats))]
-         (doseq [trig-time trigger-times]
-           (apply-by (+ time (* (/ measure-length num-beats) trig-time)) trigger-f)))
-       (catch Exception e (prn "caught exception from sequence gen"))))
-    (apply-by next-time #'b-seq [next-time num-beats measure-length gen])))
-
-(b-seq (+ 1000 (now)) 6 2000 generators)
+(sequencer (+ 1000 (now)) 4 2000 generators)
 (stop)
--- src/wigh_figh/pattern.clj
+++ src/wigh_figh/pattern.clj
@@ -1,5 +1,5 @@
-(ns wigh-figh.pattern)
-
+(ns wigh-figh.pattern
+  (:use [overtone.live :only [apply-by]]))
 
 (defn- pat-gen [start dur xs]
   (let [num-xs (count xs)
@@ -26,3 +26,13 @@
    (map #(if (vector? %) (vec (rot-rec %)) %) (vec (rot xs))))
   ([xs n]
    (nth (iterate rot-rec xs) n)))
+
+(defn sequencer [time num-beats measure-length gen]
+  (let [next-time (+ measure-length time)]
+    (doseq [[seq-gen trigger-f] @gen]
+     (try 
+       (let [trigger-times (take-while #(< % num-beats) (seq-gen num-beats))]
+         (doseq [trig-time trigger-times]
+           (apply-by (+ time (* (/ measure-length num-beats) trig-time)) trigger-f)))
+       (catch Exception e (prn "caught exception from sequence gen"))))
+    (apply-by next-time #'sequencer [next-time num-beats measure-length gen])))
