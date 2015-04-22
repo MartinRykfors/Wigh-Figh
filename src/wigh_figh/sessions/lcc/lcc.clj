46c5face1d8d98fc814994640341cac778c14474
Martin Rykfors <martinrykfors@gmail.com>
2015-04-22 19:23:14 +0100

    Prepare lcc for how the demo should start out

--- src/wigh_figh/sessions/lcc/lcc.clj
+++ src/wigh_figh/sessions/lcc/lcc.clj
@@ -17,76 +17,27 @@
     (let [env (env-gen (env-perc 0 time))]
       (* env (sin-osc (lin-lin env 0 1 end-freq start-freq))))))
 
-(defsynth kick []
-  (->> [[0.02 2000 200]
-        [1 300 70]
-        [1 50 20]]
+(defsynth kick [amp 0.5]
+  (->> [[0.01 3000 200]
+        [0.3 170 70]
+        [0.6 80 20]]
        (map #(apply chirp %))
        (mix)
        (#(* (- 1 (detect-silence % 0.0001 0.1 FREE)) %))
+       (* amp)
        (out [0 1])))
 
-(defsynth fm [note 40 time1 1 mult1 4 time2 2.7 index1 3 ffreq 1000 mult2 3 time3 0.3 index2 3 hfreq 20]
-  (let [base-freq (midicps note)]
-    (->> (sin-osc (* mult2 base-freq))
-         (* (env-gen (env-perc 0.0 time3) ))
-         (* index2)
-         (sin-osc (* mult1 base-freq))
-         (* (env-gen (env-perc 0.0 time2) ))
-         (* index1)
-         (sin-osc base-freq)
-         (* (env-gen (env-perc 0.1 time1) :action FREE))
-         (#(lpf % ffreq))
-         (#(hpf % hfreq))
-         (out [0 1]))))
 
-(defn play-note []
-  (doseq [note (chord :c3 :minor)]
-    (fm :note note
-        :time1 (fader 0 2 "-----#------------")
-        :time2 (fader 0 2 "----#-------------")
-        :time3 (fader 0 2 "--------#---------")
-        :mult1 5
-        :mult2 4
-        :index1 (fader 0 3 "-----#------------")
-        :index2 (fader 0 9 "--------#---------")
-        :ffreq (fader 100 17000 3 "-------#----------"))))
 
-(defn snare []
-  ( fm :note 20
-       :time1 (fader 0 1 "-------#----------")
-       :time2 (fader 0 1 "---------#--------")
-       :time3 (fader 0 0.3 "----#-------------")
-       :mult1 5.3
-       :mult2 100.334
-       :index1 (fader 0 30 "-----#------------")
-       :index2 (fader 0 30 "------------#-----")
-       :ffreq (fader 100 17000 3 "---------------#--")
-       :hfreq (fader 100 17000 3 "-------#----------")
-       ))
 
-(defn hihat []
-  ( fm :note 30
-       :time1 (fader 0 2 "--------#---------")
-       :time2 (fader 0 1 "--#---------------")
-       :time3 (fader 0 2 "------------#-----")
-       :mult1 20.3
-       :mult2 30
-       :index1 (fader 0 9 "---------------#--")
-       :index2 (fader 0 9 "----------------#-")
-       :ffreq (fader 100 17000 3 "-----------------#")
-       :hfreq (fader 100 17000 3 "--------------#---")
-       ))
+
 
 
 (defonce gen (atom nil))
 
 (reset! gen
         [
-         [(pattern [4]) #(do (play-note) (transition-state!) (kick)  (hihat) )]
-         [(pattern [0]) #(do (snare) )]
-         [(pattern [0]) #(do (hihat) )]
-         [(pattern [0]) #(do (kick)  )]
+         [(pattern [4]) #(do (kick))]
          ])
 
 (sequencer (+ 1000 (now)) 4 3000 gen )
