a2d137df9b4d3efe08dfd1b052236253e0a6b75c
Martin Rykfors <martinrykfors@gmail.com>
2015-05-01 11:41:00 +0100

    Starting point for the lcc performance

--- src/wigh_figh/sessions/lcc/lcc.clj
+++ src/wigh_figh/sessions/lcc/lcc.clj
@@ -2,7 +2,7 @@
   (:use [overtone.live]
         [wigh-figh.fader]
         [wigh-figh.pattern]
-;        [wigh-figh.visuals.laser]
+        [wigh-figh.visuals.laser]
         ))
 
 (defn chirp [time start-freq end-freq]
@@ -20,16 +20,73 @@
         _ (detect-silence sig 0.0001 0.1 FREE)]
     (out [0 1] sig)))
 
+(defsynth fm [note 60 amp 0.1 attack 0.1 decay1 1 index1 3 decay2 1 mult1 3 mult2 3 index2 3 decay3 1 ffreql 1000 ffreqh 20]
+  (let [bfreq (midicps note)]
+    (->>
+     (sin-osc (* mult2 bfreq))
+     (* index2)
+     (* (env-gen (env-perc 0 decay3) ))
+     (sin-osc (* mult1 bfreq))
+     (* index1)
+     (* (env-gen (env-perc 0 decay2) ))
+     (sin-osc bfreq)
+     (* (env-gen (env-perc attack decay1) :action FREE))
+     (#(lpf % ffreql))
+     (#(hpf % ffreqh))
+     (* amp)
+     (out [0 1]))))
 
+(play-chord)
+(defn play-chord []
+  (doseq [note (chord :c3 :minor)]
+    (fm
+     ;:attack 0.1
+     :decay1 (fader 0 2 "----#-------------")
+     :decay2 (fader 0 2 "----#-------------")
+     :decay3 (fader 0 2 "----#-------------")
+     :ffreql (fader 300 17000 3 "-------------#----")
+     :index1 (fader 0 10 "----#-------------")
+     :index2 (fader 0 10 "----#-------------")
+     :mult1 3
+     :mult2 3
+     :note note)))
 
+(defn snare []
+  (fm
+     :attack 0
+     :decay1 (fader 0 2 "---#--------------")
+     :decay2 (fader 0 2 "--------------#---")
+     :decay3 (fader 0 2 "--------------#---")
+     :ffreql (fader 300 17000 3 "--------------#---")
+     :ffreqh (fader 300 17000 3 "-----#------------")
+     :index1 (fader 0 10 "----------------#-")
+     :index2 (fader 0 10 "---------------#--")
+     :mult1 20.23
+     :mult2 50.32
+     ))
 
-
+(hihat)
+(defn hihat []
+  (fm
+     :attack 0
+     :decay1 (fader 0 2 "-#----------------")
+     :decay2 (fader 0 2 "--------------#---")
+     :decay3 (fader 0 2 "----------#-------")
+     :ffreql (fader 300 17000 3 "----------------#-")
+     :ffreqh (fader 300 17000 3 "------------#-----")
+     :index1 (fader 0 10 "--------------#---")
+     :index2 (fader 0 10 "---------------#--")
+     :mult1 20.23
+     :mult2 50.32
+     ))
 
 (defonce gen (atom nil))
 
 (reset! gen
         [
-         [(pattern [4]) #(do (kick))]
+         [(pattern [1 0 [2 0] 0]) #(do (play-chord) (transition-state!) (kick))]
+         [(pattern [[0 1 1 2] [0 1 1 2] [0 1 1 2] [0 1 1 2]]) #(do (hihat))]
+         [(pattern [[0 1] [0 1] [0 1] [0 1]]) #(do (snare))]
          ])
 
 (sequencer (+ 1000 (now)) 4 3000 gen )
