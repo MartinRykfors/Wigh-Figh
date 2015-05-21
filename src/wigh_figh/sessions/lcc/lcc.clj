315ad198d658f7d388529e32a8deb68c93f7977e
Martin Rykfors <martinrykfors@gmail.com>
2015-05-21 21:08:22 +0100

    Update lcc session to use the new pattern

--- src/wigh_figh/sessions/lcc/lcc.clj
+++ src/wigh_figh/sessions/lcc/lcc.clj
@@ -2,6 +2,7 @@
   (:use [overtone.live]
         [wigh-figh.fader]
         [wigh-figh.pattern]
+        [wigh-figh.sequencer]
         [wigh-figh.visuals.laser]
         ))
 
@@ -36,36 +37,34 @@
      (* amp)
      (out [0 1]))))
 
-(play-chord)
 (defn play-chord []
   (doseq [note (chord :c3 :minor)]
     (fm
      ;:attack 0.1
-     :decay1 (fader 0 2 "----#-------------")
-     :decay2 (fader 0 2 "----#-------------")
-     :decay3 (fader 0 2 "----#-------------")
-     :ffreql (fader 300 17000 3 "-------------#----")
-     :index1 (fader 0 10 "----#-------------")
-     :index2 (fader 0 10 "----#-------------")
-     :mult1 3
-     :mult2 3
+     :decay1 (fader 0 2 "--#---------------")
+     :decay2 (fader 0 2 "------#-----------")
+     :decay3 (fader 0 2 "------#-----------")
+     :ffreql (fader 300 17000 3 "----------#-------")
+     :index1 (fader 0 10 "---#--------------")
+     :index2 (fader 0 10 "----------------#-")
+     :mult1 (+ 2 (rand-int 3))
+     :mult2 (+ 3 (rand-int 5))
      :note note)))
 
 (defn snare []
   (fm
      :attack 0
-     :decay1 (fader 0 2 "---#--------------")
-     :decay2 (fader 0 2 "--------------#---")
-     :decay3 (fader 0 2 "--------------#---")
-     :ffreql (fader 300 17000 3 "--------------#---")
-     :ffreqh (fader 300 17000 3 "-----#------------")
-     :index1 (fader 0 10 "----------------#-")
+     :decay1 (fader 0 2 "----#-------------")
+     :decay2 (fader 0 2 "-----------#------")
+     :decay3 (fader 0 2 "-----#------------")
+     :ffreql (fader 300 17000 3 "------------#-----")
+     :ffreqh (fader 300 17000 3 "----#-------------")
+     :index1 (fader 0 10 "-----------------#")
      :index2 (fader 0 10 "---------------#--")
      :mult1 20.23
      :mult2 50.32
      ))
 
-(hihat)
 (defn hihat []
   (fm
      :attack 0
@@ -84,11 +83,12 @@
 
 (reset! gen
         [
-         [(pattern [1 0 [2 0] 0]) #(do (play-chord) (transition-state!) (kick))]
-         [(pattern [[0 1 1 2] [0 1 1 2] [0 1 1 2] [0 1 1 2]]) #(do (hihat))]
-         [(pattern [[0 1] [0 1] [0 1] [0 1]]) #(do (snare))]
+         [(pattern [(i [1 (c 3 [1 0 0 (c 1 2)])] (c 3 [1 (i 0 2) 0 (i 2 1)])) (i [1 0 0 1] [2 [0 1]]) (i 3 [2 (c 0 2)]) (i 1 [1 1] 1 (c [1 3] [0 1 1 2]))]) #(do (play-chord) (transition-state!) (kick))]
+         [(pattern [[(i 2 0) 1 1 2] [0 1 1 2] [0 1 1 2] [2 1 1 2]]) #(do (hihat))]
+         [(pattern [(r 3 [0 1]) (c 6 [2 4] [4 2] [1 [2 1]] )]) #(do (snare))]
          ])
 
-(sequencer (+ 1000 (now)) 4 3000 gen )
+(sequencer (+ 1000 (now)) 0 3000 gen )
 (stop)
+(transition-state!)
 
