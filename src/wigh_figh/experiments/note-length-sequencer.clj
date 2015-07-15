8a4e274f17ff1d39e14383d9bfb345389a04b6c4
Martin Rykfors <martinrykfors@gmail.com>
2015-07-15 20:01:18 +0100

    Add nicer interface to the sequencer

--- src/wigh_figh/experiments/note-length-sequencer.clj
+++ src/wigh_figh/experiments/note-length-sequencer.clj
@@ -1,6 +1,7 @@
 (ns wigh-figh.experiments.note-length-sequencer
   (:use [overtone.live]
         [wigh-figh.pattern]
+        [wigh-figh.fader]
         [wigh-figh.sequencer]))
 
 (defsynth hit [note 40 gate 1 index 2 ffreql 500 lfo 0.5]
@@ -35,6 +36,7 @@
   (ctl h-i :gate 0))
 
 (ctl h-i :note 60)
+(ctl h-i :ffreql (fader 300 17000 3 "----------#-------"))
 (turn-off)
 (turn-on)
 
@@ -42,9 +44,11 @@
 
 (reset! gen
         [
-         [:hold (i [1/8 1/4 0 1/16 ] 0.8) turn-on turn-off]
+         [:hold (i [(r 2 1/8) 0 1/16 ] 0.8) turn-on turn-off]
          [:pattern [[1 1 0 1] 2 2 2] hihat]
          ])
 
-(sequencer (+ 500 (now)) 0 4000 gen)
+(sequencer (+ 600 (now)) 0 4000 gen)
+(def s (run-sequencer 120 4 gen))
+(kill s) ;does not work
 (stop)
--- src/wigh_figh/sequencer.clj
+++ src/wigh_figh/sequencer.clj
@@ -1,6 +1,6 @@
 (ns wigh-figh.sequencer
   (:require [wigh-figh.pattern :as p])
-  (:use [overtone.live :only [apply-by]]))
+  (:use [overtone.live :only [apply-by now]]))
 
 (defmulti schedule
   (fn [type measure-index & rest] type))
@@ -25,3 +25,8 @@
       (catch Exception e (prn (str "Sequence exn: " (.getMessage e))))))
   (let [next-time (+ measure-length time)]
     (apply-by next-time #'sequencer [next-time (inc measure-index) measure-length gen])))
+
+(defn run-sequencer [bpm signature generator-atom]
+  (let [ms-per-beat (* 1000 (/ 60 bpm))
+        measure-length (* signature ms-per-beat)]
+    (sequencer (+ 600 (now)) 0 measure-length generator-atom)))
